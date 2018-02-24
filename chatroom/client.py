06354f9cdc0757652b1ff8a88beceff459039ba2
Anusha Datar <anushadatar@gmail.com>
2018-02-24 14:59:07 -0500

    added chatroom

---
+++ chatroom/client.py
@@ -0,0 +1,31 @@
+# Client side of chat room. 
+
+import socket
+import select
+import sys
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ip_address = str(raw_input("What is the IP address?"))
+port =  int(raw_input("What is the port number?"))
+
+server.connect((ip_address, port))
+
+# Continue indefinitely. 
+while True:
+    sockets = [sys.stdin, server]
+ 
+    read_sockets,write_socket, error_socket = select.select(sockets,[],[])
+ 
+    for socks in read_sockets:
+        if socks == server:
+            message = socks.recv(2048)
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            server.send(message)
+            sys.stdout.write("You : ")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+
+server.close()
---
+++ chatroom/server.py
@@ -0,0 +1,66 @@
+# Server side of chat room. 
+import socket
+import select
+import sys
+from thread import *
+
+# Some initial setup 
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Prompt user running the program to start the server. Could be 
+# hardcoded or included as a command line argument as well.
+ip_address = str(raw_input("What is the IP address?"))
+port = int(raw_input("What is the port number?"))
+server.bind((ip_address, port))
+ 
+
+# Set the number of clients allowed.
+number_of_clients = 20
+server.listen(number_of_clients)
+clients = []
+ 
+
+def client_thread(connection, address):
+    """
+    Starts the client thread as client programs try to access the server.
+    
+    connection : The connection instance associated with the client.
+    address : The address instance associated with the client.
+    """
+    connection.send("Chatroom instantiated.")
+ 
+    # Continue indefinitely.
+    while True:
+            try:
+                message = connection.recv(2048)
+                if message:
+ 
+                    broadcast_message = address[0] + " : " + message
+                    print(broadcast_message)
+                    broadcast(broadcast_message, connection)
+                else:
+                    remove(connection)
+            except:
+                continue
+
+def broadcast(message, connection):
+    for client in clients:
+        if client!=connection:
+            try:
+                client.send(message)
+            except:
+                client.close()
+                if connection in list_of_clients:
+                    clients.remove(connection)
+ 
+# kick off client threads for each connection
+while True:
+    connection, address = server.accept()
+    clients.append(connection)
+    print address[0] + " has been connected"
+ 
+    start_new_thread(client_thread,(connection,address))    
+ 
+connection.close()
+server.close()
